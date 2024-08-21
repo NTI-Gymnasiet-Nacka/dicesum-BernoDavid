cd176a303960094f8cf7912811c8f21248edd3fd
BernoDavid <david.berno@elev.ga.ntig.se>
2024-08-21 11:08:15 +0200

    inte klar

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: David Berno
+# Datum: 2024-08-21
 
 def main():
     user_input = input().split(" ")
+    dice_one = int(user_input[0])
+    dice_two = int(user_input[1])
+
+    for d in range(1, dice_one + 1):
+        for g in range(1, dice_two + 1):
+            print(d+g)
 
 if __name__ == "__main__":
     main()
